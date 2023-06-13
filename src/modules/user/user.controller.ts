cf29f933efba657ef2aaecc84829901aedf5f459
William Daza <williamdaza04@gmail.com>
2023-06-12 19:56:32 -0500

    encrypt data

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Res, HttpStatus, Body, Get } from '@nestjs/common';
+import { Controller, Post, Res, HttpStatus, Body, Get, Req } from '@nestjs/common';
 import {CreateUserDTO} from './dto/user.dto'; 
 import { UserService } from './user.service';
+import * as CryptoJS from 'crypto-js';
 
 @Controller('user/')
 export class UserController {
@@ -8,8 +9,10 @@ export class UserController {
     constructor( private userService: UserService) {}
 
     @Post('create-user')
-    async signUp(@Res() res, @Body() createUserDTO: CreateUserDTO){
-        const user = await this.userService.createUser(createUserDTO)
+    async signUp(@Res() res, @Body() createUserDTO){
+        const decryptedData: CreateUserDTO = JSON.parse(CryptoJS.AES.decrypt(createUserDTO.encryptedData, 'coconice').toString(CryptoJS.enc.Utf8));
+        const passEncrypt = CryptoJS.AES.encrypt(decryptedData.password, 'coconut').toString();      
+        const user = await this.userService.createUser({...decryptedData, password: passEncrypt});
         return res.status(HttpStatus.OK).json({
             message: "User Successfully Created",
             user: user
@@ -17,8 +20,10 @@ export class UserController {
     }
 
     @Post('login-user')
-    async login(@Res() res, @Body() createUserDTO: CreateUserDTO){
-        const currentUser = await this.userService.loginUser(createUserDTO)
+    async login(@Res() res, @Body() createUserDTO){
+        const decryptedData: CreateUserDTO = JSON.parse(CryptoJS.AES.decrypt(createUserDTO.encryptedData, 'coconice').toString(CryptoJS.enc.Utf8));
+        const currentUser = await this.userService.loginUser(decryptedData);
+        
         return res.status(HttpStatus.OK).json({
             message: "User Successfully Logged",
             currentUser: currentUser
